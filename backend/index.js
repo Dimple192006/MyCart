e1ca7e7ea7e71b007a035efde2d93dee28dcf184
Dimple192006 <dimpleaggarwal192006@gmail.com>
2025-11-23 18:12:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let app=express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:true,
+    origin:"https://mycart-frontendseven.onrender.com",
     credentials:true
 }));
 
